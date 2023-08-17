1b8e087dd5ce680613cdce9a01acb95634bca27c
Mazespin0 <mazespin@bk.ru>
2023-08-17 17:21:58 +0800

    pank_c_4

--- c/pank_c/laba_4/laba_4/laba_4.c
+++ c/pank_c/laba_4/laba_4/laba_4.c
@@ -7,20 +7,29 @@ void setup(){
 	DDRB  |= (1 << PB3);
 	
 	TCCR2 |= (1 << WGM20) | (1 << WGM21);
-	TCCR2 |= (1 << COM21) | (1 << CS20)|(0 << CS21) | (0 << CS22);
+	TCCR2 |= (1 << COM21) | (1 << CS20)|(1 << CS21) | (0 << CS22);
 	
 	TCCR0 |= (1 << WGM00) | (1 << WGM01);
-	TCCR0 |= (1 << COM01) | (1 << CS00)|(0 << CS01) | (0 << CS02);
+	TCCR0 |= (1 << COM01) | (1 << CS00)|(1 << CS01) | (0 << CS02);
 }
       
 int main ()
 {
 	setup();
     while (1){
-		for(uint8_t duty=0, _duty=128; duty<128, _duty>0; duty++, _duty--){
+		uint8_t duty=0; 
+		uint8_t _duty=255;
+		
+		for(duty, _duty; duty<255, _duty>0; duty++, _duty--){
 			OCR0=duty;
 			OCR2=_duty;
-			_delay_ms(1);
-			}
+			_delay_ms(5);
+		}
+			
+		for(duty, _duty; _duty<255, duty>0; _duty++, duty--){
+			OCR0=duty;
+			OCR2=_duty;
+			_delay_ms(5);
+		}
 	}			
 }
