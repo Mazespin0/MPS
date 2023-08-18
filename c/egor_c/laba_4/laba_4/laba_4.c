c8fd8dbfd5220befcfb74694d5695ecc358e3e1c
Mazespin0 <mazespin@bk.ru>
2023-08-18 19:47:04 +0800

    в секундах считает, в ms не хочет

--- c/egor_c/laba_4/laba_4/laba_4.c
+++ c/egor_c/laba_4/laba_4/laba_4.c
@@ -9,10 +9,11 @@
 #include <util/delay.h>
 
 uint16_t ubrr_value = F_CPU / 16 / BAUD_RATE - 1;
-uint16_t TIM1 = 31250; 
+uint16_t TIM1 = 31250;
+unsigned long durationMillis = 0;
 
 void setup(){
-	DDRA = (0 << 0);
+	DDRD = (0 << 2);
 	
 	UBRRH = (ubrr_value >> 8) & 0xFF;
 	UBRRL = ubrr_value & 0xFF;
@@ -23,18 +24,43 @@ void setup(){
 	TIMSK = (1 << TOIE1);
 	TIFR = (1 << TOIE1);
 	TCCR1B = (1 << CS12) | (0 << CS11) | (0 << CS10);
+	GICR = (1 << INT0);
+	MCUCR = (0 << ISC00) | (1 << ISC01);
 }
 
 int main(){
 	setup();
-	while ( 1 ){
-		while (PINA == 0);
-		
-		
+	
+	while (1){
+		while(PIND == 0x00);
+	
+		TCNT1 = TIM1;
+		sei();
+		while(1);	
 	}
 }
 
-ISR(TIMER1_OVF_vect)
-{
-	
+ISR(TIMER1_OVF_vect){
+	cli();
+	durationMillis++;
+	TCNT1 = TIM1;
+	sei();
+}
+
+ISR(INT0_vect){
+	cli();
+	char buffer[100];
+    snprintf(buffer, sizeof(buffer), "Duration: %lu ms\r\n", durationMillis);
+    for (int i = 0; buffer[i] != '\0'; i++) {
+		USART_sendChar(buffer[i]);
+	}
+	durationMillis = 0;
+	//USART_sendChar('\r');
+	sei();
+}
+
+
+void USART_sendChar(char character){
+	while( !((UCSRA >> UDRE) & 1) );
+	UDR = character;
 }
